e431adde08ff43812b5f67244c6271bf90d88ad0
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2024-12-02 18:09:24 -0500

    made changes to menu navbar for easy resume access

--- app/components/containers/Navbar.tsx
+++ app/components/containers/Navbar.tsx
@@ -5,6 +5,7 @@ import {motion, AnimatePresence} from "framer-motion";
 import { useState, useEffect } from 'react';
 
 const links = [
+    { href: '/resume.pdf', text: 'RESUME' },
     { href: '/about', text: 'ABOUT' },
     { href: '/contact', text: 'CONTACT' },
     { href: '/hobbies', text: 'HOBBIES' },
@@ -24,6 +25,29 @@ export default function Navbar() {
         setStatus(!status); 
     };
 
+    const handleNavbarLinks = (link: {href: string, text: string}, index: number): JSX.Element => {
+        if(link.text == "RESUME"){
+            return(
+                <motion.a whileHover={{scale: 1.2, rotate: -360, transition: { duration: 1.3 }}} whileTap={{ scale: 1.0 }} onClick={(e) => {e.preventDefault(); router.push(link.href);}} key={index} href={link.href} className="bg-blue-700 text-white rounded-md border-white border px-2 py-1 hover:bg-white hover:text-blue-700 hover:border-black ">
+                    <div className='flex relative gap-1 justify-start items-center max-w-full max-h-full'>
+                        <img className="size-5 absolute w-full h-full" src="/resumeBg.svg" alt="M emerald"/>
+                        <span className='z-10 font-bold'>{link.text}</span>
+                    </div>  
+                </motion.a>
+            )
+        }
+        else{
+            return(
+                <motion.a whileHover={{scale: 1.2, rotate: 360, transition: { duration: 1 }}} whileTap={{ scale: 0.8 }} onClick={(e) => {e.preventDefault(); router.push(link.href);}} key={index} href={link.href} className="bg-black text-white rounded-md border-white border px-2 py-1 hover:bg-white hover:text-black hover:border-black ">
+                    <div className='flex relative gap-1 justify-start items-center max-w-full max-h-full'>
+                        <img className="size-5 absolute w-full h-full" src="/emeralds.svg" alt="emeralds"/>
+                        <span className='z-10 font-bold'>{link.text}</span>
+                    </div>  
+                </motion.a>
+            )
+        }
+    }
+
     useEffect(() => {    
         setWidth(window.innerWidth);
         
@@ -41,14 +65,10 @@ export default function Navbar() {
             {
                 width >= 480 && (
                 <nav className='size-full flex justify-around items-center gap-2'>
-                    {links.map((link, index) => (
-                        <motion.a whileHover={{scale: 1.2, rotate: 360, transition: { duration: 1 }}} whileTap={{ scale: 0.8 }} onClick={(e) => {e.preventDefault(); router.push(link.href);}} key={index} href={link.href} className="bg-black text-white rounded-md border-white border px-2 py-1 hover:bg-white hover:text-black hover:border-black ">
-                            <div className='flex relative gap-1 justify-start items-center max-w-full max-h-full'>
-                                <img className="size-5 absolute w-full h-full" src="/emeralds.svg" alt="emeralds"/>
-                                <span className='z-10 font-bold'>{link.text}</span>
-                            </div>  
-                        </motion.a>
-                    ))}
+                    {links.map((link, index) => {
+                        return handleNavbarLinks(link, index);
+                        })
+                    }
                 </nav>      
                 )
             }
@@ -61,14 +81,10 @@ export default function Navbar() {
                                 status == true && (
                                 <motion.div onClick={() => {setStatus(!status);}} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ ease: "easeIn", duration: 0.3 }} className="bg-black/20 backdrop-blur-md fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-20">
                                 <nav className='size-full flex flex-col justify-around items-center gap-2'>
-                                    {links.map((link, index) => (
-                                        <motion.a whileHover={{scale: 1.2, rotate: 360, transition: { duration: 1 }}} whileTap={{ scale: 0.8 }} onClick={(e) => {e.preventDefault(); router.push(link.href);}} key={index} href={link.href} className="bg-black text-white rounded-md border-white border px-2 py-1 hover:bg-white hover:text-black hover:border-black ">
-                                            <div className='flex relative gap-1 justify-start items-center max-w-full max-h-full'>
-                                                <img className="size-5 absolute w-full h-full" src="/emeralds.svg" alt="emeralds"/>
-                                                <span className='z-10 font-bold'>{link.text}</span>
-                                            </div>  
-                                        </motion.a>
-                                    ))}
+                                    {links.map((link, index) => {
+                                        return handleNavbarLinks(link, index);
+                                        })
+                                    }
                                 </nav>   
                                 </motion.div>
                                 )
