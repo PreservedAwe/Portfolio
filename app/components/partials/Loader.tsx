b335c4ea18b4685166400717d895a9d6b6b3f082
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2024-12-20 10:42:15 -0500

    loader time changed

--- app/components/partials/Loader.tsx
+++ app/components/partials/Loader.tsx
@@ -9,14 +9,14 @@ export default function Loader() {
     const [showLoader, setShowLoader] = useState(true);
 
     useEffect(() => {
-        const timer = setTimeout(() => setShowLoader(false), 1500);
+        const timer = setTimeout(() => setShowLoader(false), 3000);
         return () => {clearTimeout(timer)};
     })
 
     return (
         <AnimatePresence >
             { showLoader && (<motion.div initial={{opacity: 0}} animate={{opacity: 1 }} exit={{opacity: 0}} transition={{ duration: 0.15, ease: "easeOut"}} className="h-screen w-screen flex flex-col justify-center items-center bg-main-theme z-50 fixed">
-                <motion.img initial={{y: -1000, rotate: 90}} animate={{ y: 1000 }} transition={{ repeat: Infinity, duration: 1.5, ease: "linear" }} src="/loading.svg" alt="loading" className="size-96" />
+                <motion.img initial={{y: -1000, rotate: 90}} animate={{ y: 1000 }} transition={{ repeat: Infinity, duration: 3, ease: "linear" }} src="/loading.svg" alt="loading" className="size-96" />
                 <Text.LoadingText text="Loading..."/>
                 {
                 /* Switch Out motion.img and img based on Framer Motion's Animations. 
