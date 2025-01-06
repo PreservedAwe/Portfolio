20be443c9ee6d4bc8f818a1e98163b4607bf9c74
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2025-01-06 13:50:46 -0500

    more tests2

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-    return await ValidateAdmin.checkIfNotAdmin(request.nextUrl.origin);
+    //return await ValidateAdmin.checkIfNotAdmin(request.nextUrl.origin);
+    return await ValidateAdmin.checkIfNotAdmin("https://nasean.dev:443");
 }
 
 // See "Matching Paths" below to learn more
