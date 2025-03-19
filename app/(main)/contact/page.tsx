41f37e39f3c6754be5ca10c241bc389c77c4df2b
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2025-03-19 01:56:52 -0400

    small change to contact page

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -3,13 +3,14 @@ import ContactForm from "@/components/containers/ContactForm";
 import * as Text from "@/components/text/Text";
 import {memo} from 'react';
 
+const emailReceiver: string = process.env.MAIL_RECEIVER_EMAIL || "bant7ds@yahoo.com";
 const MainContent = () => {
 
   return(
     <main className={ContentType.mainContent}>
       <div className="flex flex-col max-h-full max-w-full overflow-y-hidden overflow-x-hidden text-white rounded border-white p-14 gap-3">
         <Text.LText text="Send a message here or email@:"/>
-        <Text.MText text="naseanlawson @outlook.com"/>
+        <Text.MText text={emailReceiver}/>
         <ContactForm />
       </div>
     </main>
