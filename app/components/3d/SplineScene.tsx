e0e2b00e04258a3e456f6fffe5d7e5ab1c83a9b2
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2025-03-26 21:34:04 -0400

    spline remove change

--- app/components/3d/SplineScene.tsx
+++ app/components/3d/SplineScene.tsx
@@ -1,10 +1,10 @@
-import Spline from '@splinetool/react-spline/next';
+//import Spline from '@splinetool/react-spline/next';
 
 export default function Scene() {
     return (
         <>
             <div className="fixed top-0 left-0 w-screen h-screen -z-10">
-                <Spline scene="https://prod.spline.design/DaqJ1nOmvbogfCna/scene.splinecode" />
+
             </div>
         </>
     );
