b28f73dc68c84f4c317935c59d17bbff6d5587c2
PreservedAwe <154258651+PreservedAwe@users.noreply.github.com>
2025-04-29 15:55:19 -0400

    fixed login page for malicious requests

--- app/(main)/admin/login/page.tsx
+++ app/(main)/admin/login/page.tsx
@@ -4,15 +4,26 @@ import { headers } from 'next/headers';
 import { Suspense, memo } from "react";
 
 export default memo(async function Page() {
+    const allowedHosts = ['preserved.app', 'localhost:3000'];
+
     const origin = async() => {
         const headerList = await headers();
         
         const protocol = headerList.get('x-forwarded-proto') || '';
         const hostname = headerList.get('x-forwarded-host') || '';
-    
+
+        const isAllowed = allowedHosts.some(allowed => hostname.endsWith(allowed));
+
+        if (!isAllowed) {
+            if (protocol == 'http'){
+                return `${protocol}://${allowedHosts[1]}`;
+            }
+            else if (protocol == 'https'){
+                return `${protocol}://${allowedHosts[0]}`;
+            }
+        }
         return `${protocol}://${hostname}`;
     }
-    
     await ValidateAdmin.checkIfAdmin(await origin())
 
     return (
